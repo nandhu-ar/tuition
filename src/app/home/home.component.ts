0ac6bec9c7be584f45b9a009220d6298b80afbf0
Ubuntu <windows@udemy.ynyyxffja0le5preyzk15oqc0h.tx.internal.cloudapp.net>
2024-04-06 13:20:54 +0000

    home page skeleton and theme change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+    public subjectsOffered = [
+      'English',
+      'Hindi',
+      'Maths',
+      'Science'
+    ]
 
+    public reasonToChooseMe = [
+      'Reason 1',
+      'Reason 2',
+      'Reason 3',
+      'Reason 4'
+    ]
 }
