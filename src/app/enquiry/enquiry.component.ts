26f4d7b17016c6832bf9323be9e4a4731a835daf
Nandhana A Regi <nandhanaaregi@gmail.com>
2024-04-07 16:20:51 +0530

    added email service

--- src/app/enquiry/enquiry.component.ts
+++ src/app/enquiry/enquiry.component.ts
@@ -1,29 +1,30 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { EmailService } from "../services/email.service";
 
 @Component({
-  selector: 'app-enquiry',
-  templateUrl: './enquiry.component.html',
-  styleUrl: './enquiry.component.scss'
+  selector: "app-enquiry",
+  templateUrl: "./enquiry.component.html",
+  styleUrl: "./enquiry.component.scss",
 })
 export class EnquiryComponent {
-
-  public classes = ['Class 1', 'Class 2'];
+  public classes = ["Class 1", "Class 2"];
   public enquiryForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    subject: new FormControl('', [Validators.required]),
-    class: new FormControl(''),
-    contact: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.email])
+    name: new FormControl("", [Validators.required]),
+    subject: new FormControl("", [Validators.required]),
+    class: new FormControl(""),
+    contact: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
   });
 
   submit() {
     console.log(this.enquiryForm.value);
-
+    if (!this.enquiryForm.valid) return;
+    new EmailService().sendEmail(this.enquiryForm.value);
   }
 
-  get classDropDown () {
-return this.enquiryForm.controls.class.value || 'Class'
+  get classDropDown() {
+    return this.enquiryForm.controls.class.value || "Class";
   }
   resetForm() {
     this.enquiryForm.reset();
